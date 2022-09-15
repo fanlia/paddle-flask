b87c79d3ba6d2d5ff839f4d5a5337558ff9ac413
fanlia <you@example.com>
2022-09-15 13:08:25 +0800

    first commit

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from paddleocr import PaddleOCR
 
 app = Flask(__name__)
 CORS(app)
+app.config['JSON_AS_ASCII'] = False
+app.config['JSONIFY_MIMETYPE'] = "application/json;charset=utf-8"
 
 paddle_ocr = PaddleOCR(use_gpu=False, lang='ch')
 
-@app.route("/ocr", methods=['POST'])
+@app.route("/ocr", methods=['GET', 'POST'])
 def ocr_route():
-    file = request.files['file']
-    buf = file.read()
-    result = paddle_ocr.ocr(buf, cls=True)
+    if request.method == 'POST':
+        file = request.files['file']
+        buf = file.read()
+        result = paddle_ocr.ocr(buf, cls=True)
 
-    return jsonify({'result': result})
+        return jsonify({'result': result})
+    else:
+        return render_template('upload.html')
 
 if __name__ == "__main__":
     if os.getenv('FLASK_ENV') == 'production':
